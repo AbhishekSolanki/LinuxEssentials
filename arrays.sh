186ec7c1f5c878630a6d493e6326ef1c2c2a24b7
Abhishek Solanki <abhisolanki94@gmail.com>
2019-01-13 21:12:11 +0530

    arrays

--- arrays.sh
+++ arrays.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# arrays
+# array
+alphabets=(a b c d)
+# array size
+echo ${#alphabets[@]}
+# values
+echo ${alphabets[@]}
+# access by index and change value
+alphabets[0]="A"
+echo ${alphabets[0]}
 
 # associative array
+# this can be used as key value pairs
 declare -a numbers 
 numbers=( ["1"]="one" ["2"]="two" ["3"]=three )
 # value of 1
